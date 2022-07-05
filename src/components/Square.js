aeb8153e5d809f757f3c76c400baee6bc2c9bb42
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-07-05 15:05:48 -0700

    treasure-hunt-lc

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,8 +4,14 @@ class Square extends Component {
   render() {
     return(
       <>
+        <div className='square'>
+          {this.props.value}
+        </div>
       </>
     )
   }
 }
 export default Square
+
+
+
